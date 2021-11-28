013f94a7b8ff8188afc1aee57f8d0e90dd0670e9
salman156 <salman156@debian.salman>
2021-11-28 21:08:27 +0300

    race fix between data access and data allocation

--- ThreadPool/ipcontainer/ipcontainer.h
+++ ThreadPool/ipcontainer/ipcontainer.h
@@ -1,11 +1,13 @@
 #include <vector>
 #include "../threadsafe_queue/atomic_threadsafe_queue/threadsafe_queue.h"
 #include <memory>
+#include <mutex>
 
 template<typename T>
 class ipcontainer{
     static std::vector<T> container;
     static threadsafe_queue<int,64> queue;
+    static std::mutex alloc_mutex;
     static bool init = Init();
     static int Init(){
         container = vector<T>();
@@ -21,16 +23,19 @@ public:
             container[*ptr_item] = val;
             pos = *ptr_item;
         }
-        else
+        else{
+            std::scoped_lock<std::mutex> lock(alloc_mutex);
             container.push_back(val);
+        }
 
         return pos;
     }
     static int deleteIP(int pos){
         queue.push(pos);
     }
-    
+
     T& operator[](int pos){
+        std::scoped_lock<std::mutex> lock(alloc_mutex);
         return container[pos];
     }
 };
